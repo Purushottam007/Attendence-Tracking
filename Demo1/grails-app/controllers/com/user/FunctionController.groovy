3a7a525a65dfb6af90ca11997cf8bf7af2ddf3b5
Purushottam kumar Singh <ipksingh00@gmail.com>
2017-06-07 18:21:31 +0530

    Enabled Rest Api

--- Demo1/grails-app/controllers/com/user/FunctionController.groovy
+++
@@ -1,6 +0,0 @@
-package com.user
-
-class FunctionController {
-
-    def index() { }
-}
--- Demo1/out/production/Demo1/spring/resources.groovy
+++
@@ -1,5 +0,0 @@
-import com.user.UserPasswordEncoderListener
-// Place your Spring DSL code here
-beans = {
-    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
-}
